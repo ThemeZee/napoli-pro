be3f0e520f15b9616f2908dfedd1c0c3ede16bec
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-03 14:36:01 +0200

    added SVG icon to scroll to top button

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -49,9 +49,23 @@ class Napoli_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'napoli-pro-scroll-to-top', NAPOLI_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210212', true );
+			wp_enqueue_script( 'napoli-pro-scroll-to-top', NAPOLI_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210603', true );
+
+			// Passing Parameters to navigation.js.
+			wp_localize_script( 'napoli-pro-scroll-to-top', 'napoliProScrollToTop', array( 'icon' => self::get_svg( 'collapse' ) ) );
 
 		endif;
+	}
+
+	/**
+	 * Get SVG icon.
+	 *
+	 * @return void
+	 */
+	static function get_svg( $icon ) {
+		if ( function_exists( 'napoli_get_svg' ) ) {
+			return napoli_get_svg( $icon );
+		}
 	}
 
 	/**
