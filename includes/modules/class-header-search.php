0dc12dee2e2e43ec35a3addc4a2be20b7d87cb02
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-04 08:43:37 +0200

    added svg icon to header search

--- includes/modules/class-header-search.php
+++ includes/modules/class-header-search.php
@@ -80,7 +80,7 @@ class Napoli_Pro_Header_Search {
 
 			$items .= '<li class="header-search menu-item menu-item-search">';
 			$items .= '<a class="header-search-icon" aria-expanded="false" ' . self::amp_search_toggle() . '>';
-			$items .= '<span class="genericon-search"></span>';
+			$items .= self::get_svg( 'search' );
 			$items .= '<span class="screen-reader-text">' . esc_html_x( 'Search', 'napoli-pro' ) . '</span>';
 			$items .= '</a>';
 			$items .= '<div class="header-search-form" ' . self::amp_search_is_toggled() . '>';
@@ -91,6 +91,17 @@ class Napoli_Pro_Header_Search {
 		endif;
 
 		return $items;
+	}
+
+	/**
+	 * Get SVG icon.
+	 *
+	 * @return void
+	 */
+	static function get_svg( $icon ) {
+		if ( function_exists( 'napoli_get_svg' ) ) {
+			return napoli_get_svg( $icon );
+		}
 	}
 
 	/**
